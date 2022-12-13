aecd9db5ec915c75394bceaafde40c8d5456cf26
allan-oliveira <allanrj@gmail.com>
2022-12-13 21:48:07 +0000

    Adicionar arquivos de projeto.

---
+++ src/YAMongo.API/Controllers/MongoController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using YAMongo.API.Data;
+
+namespace YAMongo.API.Controllers
+{
+    public class MongoController : Controller
+    {
+        private readonly IMongoClient _mongoClient;
+        public MongoController(IMongoClient mongoClient)
+        {
+            _mongoClient = mongoClient;
+        }
+
+        [Route("~/insert")]
+        public IActionResult InsertData()
+        {
+            var key = Guid.NewGuid();
+            var person = new Person() { Id = key, Name = $"Insert - {DateTime.Now}" };
+
+            var database = _mongoClient.GetDatabase("foo");
+            var collection = database.GetCollection<Person>("persons");
+            collection.InsertOne(person);
+
+            return Ok();
+        }
+
+        [Route("~/update")]
+        public IActionResult UpdateData()
+        {
+            var key = Guid.NewGuid();
+            var person = new Person() { Id = key, Name = $"Update - {DateTime.Now}" };
+            
+            var database = _mongoClient.GetDatabase("foo");
+            var collection = database.GetCollection<Person>("persons");
+            collection.ReplaceOne(t => t.Id == key, person, new ReplaceOptions { IsUpsert = true });
+            
+            return Ok();
+        }
+
+    }
+}
---
+++ src/YAMongo.API/Data/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace YAMongo.API.Data
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name:{Name}, Id:{Id}";
+        }
+    }
+}
